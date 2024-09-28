e4d4eafb4f816320adc3579868d54a74e582e9ec
Mrinal Jain <2mrinaljain@gmail.com>
2024-09-28 17:18:30 -0400

    api calls for beds and rooms

--- triage-dashboard/src/app/page.tsx
+++ triage-dashboard/src/app/page.tsx
@@ -4,6 +4,8 @@ import ERLiveResponse from "./ERLiveResponse";
 
 export default function Home() {
   const [patients, setPatients] = useState([]);
+  const [beds, setBeds] = useState([]);
+  const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -12,6 +14,14 @@ export default function Home() {
         .then((response) => response.json())
         .then((data) => setPatients(data))
         .catch((error) => console.error("Error fetching patient data:", error));
+      fetch("http://localhost:5100/get_beds")
+        .then((response) => response.json())
+        .then((data) => setBeds(data))
+        .catch((error) => console.error("Error fetching bed data:", error));
+      fetch("http://localhost:5100/get_rooms")
+        .then((response) => response.json())
+        .then((data) => setRooms(data))
+        .catch((error) => console.error("Error fetching bed data:", error));
     }, 1000);
     // Clear the interval on component unmount
     return () => clearInterval(intervalId);
@@ -19,7 +29,7 @@ export default function Home() {
   console.log(patients);
   return (
     <main className="container">
-      <ERLiveResponse patients={patients} />
+      <ERLiveResponse patients={patients} beds={beds} rooms={rooms} />
     </main>
   );
 }
