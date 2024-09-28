f1583ce08cb4833abf0167b3e9421bfd3cf9c519
kaustubhbhal <kaustubhsbhal@gmail.com>
2024-09-27 21:13:34 -0400

    style changes

--- triage-dashboard/src/app/components/NotificationBar.js
+++ triage-dashboard/src/app/components/NotificationBar.js
@@ -5,7 +5,7 @@ import styles from './NotificationBar.module.css';
 const NotificationBar = () => {
   return (
     <div className={styles.notificationBar}>
-      <h2 className={styles.title}>Notifications</h2>
+      <h2 className={styles.title}>Patients</h2>
       <ul className={styles.notificationList}>
         <li>Emergency Alert: Room 101 is now available.</li>
         <li>New Patient Arrived: Room 102 is occupied.</li>
